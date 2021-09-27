86c581da0801d38bce3bf1cc4720730bd11e0a88
babinicz <babinicz@onet.pl>
2021-09-27 15:22:11 +0200

    zm

--- src/components/mail/Thank.js
+++ src/components/mail/Thank.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   min-height: 100vh;
@@ -11,6 +12,7 @@ const Thank = () => {
   return (
     <Container>
       <h1>Dziękuję za wiadomość!</h1>
+      <Button to="/">Powrót</Button>
     </Container>
   );
 };
