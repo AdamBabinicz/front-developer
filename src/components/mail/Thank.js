4e66135d082fb7c419888f6c54ea965e4107aa69
babinicz <babinicz@onet.pl>
2021-09-27 15:17:15 +0200

    zm

--- src/components/mail/Thank.js
+++ src/components/mail/Thank.js
@@ -1,10 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+`;
 
 const Thank = () => {
   return (
-    <div>
+    <Container>
       <h1>Dziękuję za wiadomość!</h1>
-    </div>
+    </Container>
   );
 };
 
