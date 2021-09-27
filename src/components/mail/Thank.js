d629013e32b48d54c3179fe81c771e28cf75f90e
babinicz <babinicz@onet.pl>
2021-09-27 15:24:43 +0200

    button

--- src/components/mail/Thank.js
+++ src/components/mail/Thank.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { Button } from "../button/index";
 
 const Container = styled.div`
   min-height: 100vh;
