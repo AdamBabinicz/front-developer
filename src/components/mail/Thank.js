2f5d0520599bdf218ac0e3ae5b48dae32bb7806a
babinicz <babinicz@onet.pl>
2021-09-27 15:40:41 +0200

    background

--- src/components/mail/Thank.js
+++ src/components/mail/Thank.js
@@ -2,12 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { Button } from "../button/index";
+import BackgroundImg from "../../assets/pictures/codeb.jpg";
 
 const Container = styled.div`
   min-height: 100vh;
   display: grid;
   place-items: center;
-  background: #333;
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.8)),
+    url(${BackgroundImg}) center/cover no-repeat fixed;
+  align-items: center;
   color: #fff;
 `;
 
